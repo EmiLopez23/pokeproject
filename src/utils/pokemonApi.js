8eb34e1c3fc5dc600b07e39e43621807a50d6f59
Emi <emiliano.lopez@ing.austral.edu.ar>
2023-08-08 00:50:02 -0300

    add methods to get all data for pokemon page

--- src/utils/pokemonApi.js
+++ src/utils/pokemonApi.js
@@ -21,7 +21,27 @@ export async function getPokemons(limit=10, page){
 
 export async function getPokemonById(id){
     const {data} = await pokemonApi.get(`/pokemon/${id}`)
-    return data
+    return {...data}
+}
+
+export async function getPokemonByIdWithEvolves(id){
+    const data = await getPokemonById(id)
+    const {data: species} = await pokemonApi.get(`/pokemon-species/${id}`)
+    const evolutionId = species.evolution_chain.url.split("/evolution-chain/")[1]
+    const evolutions = await getEvolves(evolutionId)
+    return {...data, evolutions}
+}
+
+export async function getEvolves(id){
+    const {data} = await pokemonApi.get(`/evolution-chain/${id}`)
+    let {evolves_to} = data.chain
+    let evolutionChain = [{...data.chain.species, url: data.chain.species.url.split("/pokemon-species/")[1]}]
+    while(evolves_to.length > 0){
+        evolutionChain.push({...evolves_to[0].species, url: evolves_to[0].species.url.split("/pokemon-species/")[1]})
+        evolves_to = evolves_to[0].evolves_to
+    }
+    const evolutionData = await Promise.all(evolutionChain.map(({url})=> getPokemonById(url)))
+    return evolutionData
 }
 
 export async function getTypes(){
